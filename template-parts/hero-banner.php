dfeec03b9ce63cc3c574095c8501f676263a42ed
lumiosdigital <lumiosdigital@gmail.com>
2025-08-13 11:12:09 -0300

    Update theme version to 1.7.0, replace hero banner foreground image, and remove outdated .DS_Store files

--- template-parts/hero-banner.php
+++ template-parts/hero-banner.php
@@ -83,6 +83,6 @@
 
     <!-- Foreground SVG -->
     <div class="hero-fg-svg">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-banner-foreground.png" alt="Hero Foreground" class="hero-fg-image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-banner-foreground-new.png" alt="Hero Foreground" class="hero-fg-image">
     </div>
 </section>
